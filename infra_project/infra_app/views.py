1cd34380ca69864b5ce8bd63f0badec06ea88352
AlexeyKurepin <AlexeyKurepin>
2022-09-19 21:27:06 +0300

    Add docker image to cloud.yandex

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('У меня получилось, какой же я молодец!')
+    return HttpResponse('У меня получилось! какой же я молодец!')
 
 
 def second_page(request):
